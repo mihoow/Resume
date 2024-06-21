38f44189281eea8e50c2ff636ef473a2df498768
Michał Wieczorek <michal.wieczorek@scandiweb.com>
2024-06-21 11:38:56 +0200

    Prevent react update when company data did not changed

--- app/components/RootDataProvider/RootData.provider.tsx
+++ app/components/RootDataProvider/RootData.provider.tsx
@@ -1,11 +1,31 @@
-import type { PropsWithChildren } from 'react';
+import { useState, type PropsWithChildren, useEffect } from 'react';
 import type { RootData } from '~/types/global';
 import { RootDataContext } from './RootData.context';
 import { component } from '~/utils/component';
 
 export const RootDataProvider = component<PropsWithChildren<{ data: RootData }>>(
     'RootDataProvider',
-    function ({ data, children }) {
+    function ({ data: loaderData, children }) {
+        const [data, setData] = useState(loaderData);
+
+        useEffect(() => {
+            const { isAdmin, company, allCompanies, sensitiveAuthorInfo, actionData } = loaderData;
+
+            setData((currData) => {
+                if (JSON.stringify(company) !== JSON.stringify(currData.company)) {
+                    currData.company = company;
+                }
+
+                return {
+                    ...currData,
+                    isAdmin,
+                    allCompanies,
+                    sensitiveAuthorInfo,
+                    actionData,
+                };
+            });
+        }, [loaderData]);
+
         return <RootDataContext.Provider value={data}>{children}</RootDataContext.Provider>;
     }
 );

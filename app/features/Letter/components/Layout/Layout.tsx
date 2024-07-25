ea2e91f92fe1c91459235dfeaacdde0f7c9f187c
Michał Wieczorek <michal.wieczorek@scandiweb.com>
2024-07-25 20:25:06 +0200

    Add hard-coded cover letter

---
+++ app/features/Letter/components/Layout/Layout.tsx
@@ -0,0 +1,34 @@
+import { PropsWithChildren } from 'react';
+import { component } from '~/utils/component';
+import { useTranslation } from '~/hooks/useTranslation';
+
+export const LetterLayout = component<PropsWithChildren>('LetterLayout', function ({ className, children }) {
+    const t = useTranslation();
+
+    return (
+        <div className={this.mcn(className)}>
+            <header className={this.__('Header')}>
+                <div className={this.__('Recipient')}>
+                    <span className={this.__('To')}>{t('To', 'Do')}</span>
+                    <span>James Alex Smith</span>
+                    <span>HR Manager</span>
+                    <span>Cloud Clearwater</span>
+                    <span>4292 Sugarfoot Lane, Raleigh</span>
+                    <span>North Dakota, U.S.</span>
+                </div>
+                <time
+                    dateTime='2024-06-24'
+                    className={this.__('Date')}
+                >
+                    24 June, 2024
+                </time>
+            </header>
+            <strong className={this.__('Greeting')}>Dear Mr. Smith,</strong>
+            <article className={this.__('Content')}>{children}</article>
+            <footer className={this.__('Farewell')}>
+                <span>{t('Sincerely', 'Z poważaniem')},</span>
+                <span>Michał Wieczorek</span>
+            </footer>
+        </div>
+    );
+});
